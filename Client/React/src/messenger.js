e9d243eed04e84b5029715f6deaf0e573edf8f27
Evan Manuella <EvanFmanuella@gmail.com>
2020-10-26 20:40:54 -0400

    Add signalr and remove dotnetify comnponents from messenger

--- Client/React/src/messenger.js
+++ Client/React/src/messenger.js
@@ -1,20 +1,65 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import ReactDOM from "react-dom";
+import { HubConnectionBuilder } from '@microsoft/signalr';
 
 export class Messenger extends Component {
+  
   constructor(props) {
     super(props);
     this.state = { Users: [], Messages: [], message: "" };
     this.scrollToBottom = () =>
       this.bottomElem.scrollIntoView({ behavior: "smooth" });
 
-    // this.vm = dotnetify.react.connect("MessengerVM", this);
     this.dispatchState = (state) => this.vm.$dispatch(state);
+
+    this.connection = new HubConnectionBuilder()
+      .withUrl('https://localhost:5001/messengerhub')
+      .withAutomaticReconnect()
+      .build();
+
+    if (this.connection) {
+      this.connection.start()
+        .then(result => {
+          console.log('Connected!');
+
+          this.connection.on('ReceiveMessage', (user, message) => {
+            const updatedChat = [...latestChat.current];
+            var encodedMsg = user + ':' + message
+            updatedChat.push(encodedMsg);
+
+            setChat(updatedChat);
+          });
+        })
+        .catch(e => console.log('Connection failed: ', e));
+    }
+  }
+
+
+
+  sendMessage(user, message) {
+    const chatMessage = {
+      user: user,
+      message: message
+    }
+
+    console.log("Send message : " + chatMessage );
+
+    if (this.connection.connectionStarted) {
+      try {
+        this.connection.send('SendMessage', 'UserInput', chatMessage);
+      }
+      catch (e) {
+        console.log(e);
+      }
+    }
+    else {
+      alert('No connection to server yet.');
+    }
   }
 
   componentDidMount() {
-    this.correlationId = `${Math.random()}`;
-    this.dispatchState({ AddUser: this.correlationId });
+    // this.correlationId = `${Math.random()}`;
+    // this.dispatchState({ AddUser: this.correlationId });
     this.scrollToBottom();
   }
 
@@ -41,6 +86,7 @@ export class Messenger extends Component {
 
   sendMessage(text) {
     var match = /name is ([A-z]+)/i.exec(text);
+    console.log("Send message!" );
     this.dispatchState({
       SendMessage: {
         Text: text,
@@ -112,12 +158,11 @@ export class Messenger extends Component {
   }
 }
 
-export function RenderMessenger(elementId) 
-{
-    const element = <Messenger></Messenger>;
-    var target = document.getElementById(elementId);
-    console.log('Element! ' + element);
-    console.log('Target! ' + target);
-    ReactDOM.render(element, target);
+export function RenderMessenger(elementId) {
+  const element = <Messenger></Messenger>;
+  var target = document.getElementById(elementId);
+  console.log('Element! ' + element);
+  console.log('Target! ' + target);
+  ReactDOM.render(element, target);
 }
 
